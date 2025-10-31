e3e7c31c95e79976c7145900078d90e01259fe4c
jbangerter <jbangerter04@gmail.com>
2025-10-31 00:15:12 -0600

    Phase 4:tables that need to be added implemented

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -9,9 +9,37 @@ import java.util.List;
 
 public class SqlDataAccess implements DataAccess {
 
-    public SqlDataAccess() throws DataAccessException {
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL UNIQUE,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password_hash VARCHAR(255) NOT NULL
+            )
+            """,
 
-    }
+            """
+            CREATE TABLE IF NOT EXISTS authdata (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255) NOT NULL UNIQUE,
+                authToken VARCHAR(255) NOT NULL UNIQUE
+            )
+            """,
+
+            """
+            CREATE TABLE IF NOT EXISTS Games (
+                gameID INT AUTO_INCREMENT PRIMARY KEY,
+                whiteUsername VARCHAR(50),
+                blackUsername VARCHAR(50),
+                gameName VARCHAR(100) NOT NULL,
+                gameState LONGTEXT NOT NULL
+            )
+            """
+
+
+    };
+
 
     @Override
     public void clear() {
@@ -96,5 +124,18 @@ public class SqlDataAccess implements DataAccess {
     @Override
     public Collection<GameData> listGames() {
         return List.of();
+    }
+
+    private void configureDatabase() throws ResponseException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
